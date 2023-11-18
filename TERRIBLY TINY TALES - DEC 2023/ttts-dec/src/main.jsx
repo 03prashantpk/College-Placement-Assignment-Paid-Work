c066aa4d99c9b33a674c2b50366ca55e121a55f5
Prashant Kumar <43730425+03prashantpk@users.noreply.github.com>
2023-11-18 21:21:02 +0530

    content

--- TERRIBLY TINY TALES - DEC 2023/ttts-dec/src/main.jsx
+++ TERRIBLY TINY TALES - DEC 2023/ttts-dec/src/main.jsx
@@ -1,16 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { DE } from "enally.in-credit";
-
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+import './index.css';
+import { required } from 'enally.in-credit';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <>
+    <React.Fragment>
       <App />
-      <p className='credit'> {DE} </p>
-    </>
-
+      <p dangerouslySetInnerHTML={{ __html: required }} />
+    </React.Fragment>
   </React.StrictMode>,
-)
+);
